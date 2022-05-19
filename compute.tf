52a5920a9cc541eae65fe1901abbce0522a32a62
Anthony Lazzaro <alazzaro@google.com>
2022-05-19 17:23:45 +0100

    updated compute script

--- compute.tf
+++ compute.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "terraform" {
   project      = "python-lab-329118"
-  name         = "terraform"
+  name         = "vmvm2"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
   boot_disk {
@@ -14,3 +14,9 @@ resource "google_compute_instance" "terraform" {
     }
   }
 }
+terraform {
+  backend "gcs" {
+    bucket  = "gsk_demo_tf"
+  }
+}
+
